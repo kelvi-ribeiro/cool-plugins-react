d6afb37b4b6c4c9bf5e00fa37200c61f98f4e00a
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-06 00:21:39 -0300

    react-bootstrap
    Um package bem simples de usar, que é simplesmente a componentização em React pronta dos Componentes do Bootstrap, desde: Modals, alerts, buttons, Cards, Images, Badges, Forms e etc

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 | Biblioteca | Análise |
 | - | - |
 | react-ace | Um ótimo package para simular um editor de código fonte, existe mais de 152 linguagens, o que eles chamam de modes, e mais de 37 temas disponíveis
-| prop-types | Um package que serve para inserir tipagens e obrigatoriedades nos atributos de um componente, também permite ver os warnings no console
-| react-dropzone | Esse package permite ter um input de files(DRAG & DROP) de uma forma muito ágil, não só isso, mas é possísvel passar vários parâmetros para criação do componente "dropzone", tornando o mesmo em cada aplicação, um componente muito customizado
+| prop-types | Um package que serve para inserir tipagens e obrigatoriedades nos atributos de um componente, também permite ver os warnings no console |
+| react-dropzone | Esse package permite ter um input de files(DRAG & DROP) de uma forma muito ágil, não só isso, mas é possísvel passar vários parâmetros para criação do componente "dropzone", tornando o mesmo em cada aplicação, um componente muito customizado |
+| react-bootstrap | Um package bem simples de usar, que é simplesmente a componentização em React pronta dos Componentes do Bootstrap, desde: Modals, alerts, buttons, Cards, Images, Badges, Forms e etc |
--- src/App.js
+++ src/App.js
@@ -1,45 +1,53 @@
-import React from 'react';
-import {useDropzone} from 'react-dropzone';
+import React, { Component } from "react";
+import { Button, Alert, ThemeProvider, Container, Row, Col, Image } from 'react-bootstrap';
 
-export default function Plugin(props) {
-  const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
-    getFilesFromEvent: event => myCustomFileGetter(event)
-  });
-
-  const files = acceptedFiles.map(f => (
-    <li key={f.name}>
-      {f.name} has <strong>myProps</strong>: {f.myProp === true ? 'YES' : ''}
-    </li>
-  ));
-
-  return (
-    <section className="container">
-      <div {...getRootProps({className: 'dropzone'})}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      </div>
-      <aside>
-        <h4>Files</h4>
-        <ul>{files}</ul>
-      </aside>
-    </section>
-  );
-}
-
-async function myCustomFileGetter(event) {
-  const files = [];
-  const fileList = event.dataTransfer ? event.dataTransfer.files : event.target.files;
-
-  for (var i = 0; i < fileList.length; i++) {
-    const file = fileList.item(i);
-    
-    Object.defineProperty(file, 'myProp', {
-      value: true
-    });
-
-    files.push(file);
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Buttons</h1>
+        <ThemeProvider prefixes={{ btn: 'my-btn' }}>
+          <Button variant="primary">My Button</Button>
+        </ThemeProvider>{' '}
+        <Button bsPrefix="super-btn" variant="primary">
+          Super button
+        </Button>
+        <h1>Alertas</h1>
+        <Alert variant="success">
+          <Alert.Heading>Hey, nice to see you</Alert.Heading>
+          <p>
+            Aww yeah, you successfully read this important alert message. This example
+            text is going to run a bit longer so that you can see how spacing within an
+            alert works with this kind of content.
+        </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things nice
+            and tidy.
+        </p>
+        </Alert>
+        <Container>
+          <h1>Imagens</h1>
+          <Row>
+            <Col xs={6} md={4}>
+              <Image style={style.img} src="https://cdn.ome.lt/_1KF0rLSFPxVwCR-u5XA6Mc-k4E=/1200x630/smart/extras/conteudos/goku3.jpg" rounded />
+            </Col>
+            <Col xs={6} md={4}>
+              <Image style={style.img} src="https://www.whats-on-netflix.com/wp-content/uploads/2018/09/naruto-on-netflix.jpg" roundedCircle />
+            </Col>
+            <Col xs={6} md={4}>
+              <Image style={style.img} src="https://i.pinimg.com/originals/bf/94/f2/bf94f27a6835f873c2ecc770ff9e9897.jpg" thumbnail />
+            </Col>
+          </Row>
+        </Container>;
+      </React.Fragment>
+    )
   }
-
-  return files;
 }
 
+const style =  {
+  img: {
+    maxHeight:'50vh', 
+    maxWidth:'50vh'
+  }
+}
