489b07d5c7b05cdfd7cf9432231a9ea8f364785f
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-05 23:54:20 -0300

    react-dropzone
    Esse package permite ter um input de files(DRAG & DROP) de uma forma muito ágil, não só isso, mas é possísvel passar vários parâmetros para criação do componente 'dropzone', sendo totalmente personalizado, tornando o mesmo em cada aplicação bem único, visto que isso é muito importante, pois cada aplicação vai ter uma necessidade diferente

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 | Biblioteca | Análise |
 | - | - |
 | react-ace | Um ótimo package para simular um editor de código fonte, existe mais de 152 linguagens, o que eles chamam de modes, e mais de 37 temas disponíveis
-| prop-types | Ótimo package para inserir tipagens e obrigatoriedades nos atributos de um componente, também permite ver os warnings no console
+| prop-types | Um package que serve para inserir tipagens e obrigatoriedades nos atributos de um componente, também permite ver os warnings no console
+| react-dropzone | Esse package permite ter um input de files(DRAG & DROP) de uma forma muito ágil, não só isso, mas é possísvel passar vários parâmetros para criação do componente "dropzone", tornando o mesmo em cada aplicação, um componente muito customizado
--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import PropTypes from 'prop-types';
- 
-export default class App extends React.Component {
-  render() {
-    return (<h1>Testando package prop-types</h1>)
+import {useDropzone} from 'react-dropzone';
+
+export default function Plugin(props) {
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
+    getFilesFromEvent: event => myCustomFileGetter(event)
+  });
+
+  const files = acceptedFiles.map(f => (
+    <li key={f.name}>
+      {f.name} has <strong>myProps</strong>: {f.myProp === true ? 'YES' : ''}
+    </li>
+  ));
+
+  return (
+    <section className="container">
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </section>
+  );
+}
+
+async function myCustomFileGetter(event) {
+  const files = [];
+  const fileList = event.dataTransfer ? event.dataTransfer.files : event.target.files;
+
+  for (var i = 0; i < fileList.length; i++) {
+    const file = fileList.item(i);
+    
+    Object.defineProperty(file, 'myProp', {
+      value: true
+    });
+
+    files.push(file);
   }
+
+  return files;
 }
- 
-App.propTypes = {
-  // You can declare that a prop is a specific JS primitive. By default, these
-  // are all optional.
-  optionalArray: PropTypes.array.isRequired,
-  optionalBool: PropTypes.bool,
-  optionalFunc: PropTypes.func,
-  optionalNumber: PropTypes.number,
-  optionalObject: PropTypes.object,
-  optionalString: PropTypes.string,
-  optionalSymbol: PropTypes.symbol,
-}
+
